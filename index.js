1b48e8f7ec2023d875b2753aeab3cdc3abad86c1
gssggssg <490104722@qq.com>
2024-10-29 16:38:10 +0800

    feat: v0.1.11

---
+++ index.js
@@ -0,0 +1,23 @@
+//import vue文件的name from 路径
+import Vue2FilePreview from "./src/components/PreviewFile/index.vue";
+
+const components = [Vue2FilePreview];
+
+const install = function (Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  components.forEach((component) => Vue.component(component.name, component));
+};
+
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
+}
+
+// 暴露安装方法和组件
+const Vue2FilePreviewPlugin = {
+  install,
+  Vue2FilePreview,
+};
+
+export default Vue2FilePreviewPlugin;
+export { Vue2FilePreview };
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PreviewFile></PreviewFile>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PreviewFile from './components/PreviewFile/index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PreviewFile,
   }
 }
 </script>
